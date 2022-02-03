4b1897e58e38e3c5e98c74e9a4758f07e881abdc
centg2g <centg2g@gmail.com>
2022-02-03 17:06:54 +0300

    user regsitration

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require('./config/keys').mongoURL;
