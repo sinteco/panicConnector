78ab17bd504ab1a71cd420462d941d0a25b5e698
centg2g <centg2g@gmail.com>
2022-02-03 17:23:45 +0300

    user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -36,6 +36,29 @@ router.post('/register', (req, res) => {
             })
         }
     })
-})
+});
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //find user by email
+    User.findOne({email}).then(user => {
+        if(!user){
+            return res.status(404).json({email: 'user not found'});
+        } 
+        
+        //check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if(isMatch){
+                res.json({msg: 'Success'});
+            }else{
+                return res.status(400).json({password: 'password incorrect'});
+            }
+        })
+    }
+    
+    )
+}) 
 
 module.exports = router;
