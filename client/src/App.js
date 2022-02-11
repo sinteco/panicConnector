0f14f01cd7b2b4f04373d417abd20ea7b45d3802
centg2g <centg2g@gmail.com>
2022-02-11 17:19:26 +0300

    regsitration and login page progress 1

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Regsiter from './auth/Regsiter';
+import Login from './auth/Login';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Navbar/>
         <Routes>
           <Route exact path="/" element={<Landing/>} />
+          <Route exact path='/register' element={<Regsiter/>}/>
+          <Route exact path='/login' element={<Login/>}/>
         </Routes>
         <Footer/>
       </div>
---
+++ client/src/auth/Regsiter.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+ class Regsiter extends Component {
+  render() {
+    return (
+      <div>Regsiter</div>
+    )
+  }
+}
+export default Regsiter;
