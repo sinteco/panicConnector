010b8288d147bea36c1844ce229cc90fb3c18349
centg2g <centg2g@gmail.com>
2022-02-11 16:58:57 +0300

    add react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Landing />
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Landing/>} />
+        </Routes>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
